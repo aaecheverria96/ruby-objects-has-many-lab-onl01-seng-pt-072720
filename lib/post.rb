ced05b38bb0aa75545519e4602369ce60a306e6a
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-21 18:54:46 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -28,11 +28,11 @@ def self.all
 end
 
 def author_name
-   if @author.name == true 
-     @author.name 
-   else 
-     nil 
-   end 
+   if @author.name == true
+     @author.name
+   else
+     nil
+   end
 end
 
 
