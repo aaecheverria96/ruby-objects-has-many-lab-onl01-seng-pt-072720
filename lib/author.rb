9d7d14eb3b165e924f641b79bdd76c9bde9c1564
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-21 17:40:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,8 +11,8 @@ def add_post(post)
   post.author = self
 end
 
-def add_post_by_title(title) 
-  binding.pry 
+def add_post_by_title(title)
+  binding.pry
   post = Post.new(title)
   post.author = self
 end
